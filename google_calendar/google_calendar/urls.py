ac671a5987a29613b80d6258f733956e452c4180
SUBASHKUMAR N <54300487+subashkumar-it19@users.noreply.github.com>
2023-03-02 01:38:45 +0530

    Update urls.py

--- google_calendar/google_calendar/urls.py
+++ google_calendar/google_calendar/urls.py
@@ -4,6 +4,6 @@ from .views import GoogleCalendarInitView, GoogleCalendarInitCallbackView
 urlpatterns = [
     path('rest/v1/calendar/init/', GoogleCalendarInitView.as_view(),
          name='google-calendar-init'),
-    path('rest/v1/calendar/redirect', GoogleCalendarInitCallbackView.as_view(),
+    path('rest/v1/calendar/init/redirect', GoogleCalendarInitCallbackView.as_view(),
          name='google-calendar-init-callback'),
 ]
